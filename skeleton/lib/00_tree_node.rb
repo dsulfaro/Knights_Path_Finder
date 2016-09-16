30473ddb6618d930248bdef5226c888ecb56a04f
App Academy Student <student@appacademy.io>
2016-09-16 11:35:24 -0700

    not working, updating parent=

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,5 +1,5 @@
 class PolyTreeNode
-  
+
   def initialize(value)
     @value = value
     @parent = nil
@@ -8,22 +8,39 @@ class PolyTreeNode
   end
 
   def parent
+    @parent
 
   end
 
 
   def children
-
+    @children
   end
 
 
   def value
+    @value
+  end
 
+  def add_child(child_node)
+    if child_node != nil
+      @children << child_node if !@children.include?(child_node)
+      child_node.parent = self
+    end
+  end
+
+  def remove_child(child)
+    @children.delete(child) if child != nil
+    child.parent = nil
   end
 
 
-  def parent=
-
+  def parent=(par)
+    #@parent.remove_child(self) if !@parent.nil? && @parent != par
+    @parent = par
+    return nil if par == nil
+    #@parent.add_child(self) if !@children.include?(self)
+    @parent.children << self unless @parent.children.include?(self)
   end
 
 end # exit PolyTreeNode class
