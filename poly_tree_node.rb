5d8da7f1bb1793ea794c76b6d0949177be1cab33
App Academy Student <student@appacademy.io>
2016-09-16 10:46:53 -0700

    First commit. Poly tree node skeleton.

---
+++ poly_tree_node.rb
@@ -0,0 +1,26 @@
+class PolyTreeNode
+
+  def initialize(value)
+        
+  end
+
+  def parent
+
+  end
+
+
+  def children
+
+  end
+
+
+  def value
+
+  end
+
+
+  def parent=
+
+  end
+
+end # exit PolyTreeNode class
